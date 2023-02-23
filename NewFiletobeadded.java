9fbfa903b9c2121fe0cd313441ad45c95655847b
Swapnil Dubey <dubeyswapnil44@gmail.com>
2023-02-23 20:30:23 +0900

    This is to test Push facility of git

---
+++ NewFiletobeadded.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        int a = 1;
+        int b = 2;
+        System.out.println("This is to test Push facility of git);
+    }
+
+}
