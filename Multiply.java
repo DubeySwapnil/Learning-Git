ca72e397bb7eb7d7dd7e35564a980b29b4c9b905
Swapnil Dubey <dubeyswapnil44@gmail.com>
2023-02-23 20:05:23 +0900

    Added Multiply code into my branch

---
+++ Multiply.java
@@ -0,0 +1,9 @@
+public class Sum {
+    public static void main(String[] args) {
+        System.out.println("Hello world with a new commit!");
+        int a = 1;
+        int b = 2;
+        System.out.println(a * b);
+    }
+
+}
